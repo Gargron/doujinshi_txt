f3782d1ea68495d584a92320e24390819590abf8
Eugen Rochko <eugen@zeonfederated.com>
2015-01-02 15:58:33 +0100

    Adding comments and improving syntax

--- markov.js
+++ markov.js
@@ -1,12 +1,14 @@
-var fs = require('fs'),
-  repl = require('repl'),
-  Twit = require('twit'),
+var fs   = require('fs'),
+  repl   = require('repl'),
+  Twit   = require('twit'),
   dotenv = require('dotenv');
 
 var loadFiles, buildFromCorpus, buildFromLine, nextChoice, getMarkovString, getMarkovStringForWord,
   tweetMarkovString, tweetString;
 
-var markovChain = {}, startingWords = [], r;
+var markovChain = {},
+  startingWords = [],
+  r;
 
 loadFiles = function () {
   fs.readdir(__dirname + '/data/', function (err, files) {
@@ -22,6 +24,7 @@ loadFiles = function () {
           return;
         }
 
+        // Build a Markov Chain for every source text
         buildFromCorpus(data);
       });
     });
@@ -29,8 +32,10 @@ loadFiles = function () {
 };
 
 buildFromCorpus = function (data) {
+  // Eliminate Windows linebreaks, double linebreaks
   var lines = data.toString().replace("\r", "").replace("\n\n", "\n").split("\n");
 
+  // Remove empty lines
   lines = lines.map(function (line) {
     return line.trim();
   }).filter(function (line) {
@@ -38,25 +43,31 @@ buildFromCorpus = function (data) {
   });
 
   lines.forEach(function (line) {
+    // Process each line individually
     buildFromLine(line);
   });
 };
 
 buildFromLine = function (line) {
+  // Eliminate any characters other than letters/numbers, spaces,
+  // exclamation and question marks and apostrophes
   var words = line.replace(/[^\w\s\!\?\'\-']/g, ' ').split(' ');
 
+  // Remove empty "words"
   words = words.map(function (word) {
     return word.trim();
   }).filter(function (word) {
     return word.length > 0;
   });
 
+  // Add first word of line to the list of possible starting words
   if (typeof words[0] !== 'undefined') {
     startingWords.push(words[0].toLowerCase());
   }
 
+  // Build the actual network
   for (var i = 0; i < words.length - 1; i += 1) {
-    var word = words[i].toLowerCase(),
+    var word   = words[i].toLowerCase(),
       nextWord = words[i + 1].toLowerCase();
 
     if (markovChain.hasOwnProperty(word)) {
@@ -72,7 +83,9 @@ nextChoice = function (currentWord) {
 
   possibleWords = markovChain[currentWord];
 
+  // What if we don't know of any follow-up to this word?
   if (typeof possibleWords === 'undefined') {
+    // For now: End output preemptively
     return null;
   }
 
@@ -89,7 +102,7 @@ getMarkovString = function (len) {
 
 getMarkovStringForWord = function (start, len) {
   var stringArr = [],
-    nextWord = start;
+    nextWord    = start;
 
   while (nextWord !== null && stringArr.length < len) {
     stringArr.push(nextWord);
@@ -106,9 +119,9 @@ tweetMarkovString = function () {
 
 tweetString = function (string) {
   var T = new Twit({
-    consumer_key: process.env.TWIT_CONSUMER_KEY,
-    consumer_secret: process.env.TWIT_CONSUMER_SECRET,
-    access_token: process.env.TWIT_ACCESS_TOKEN,
+    consumer_key:        process.env.TWIT_CONSUMER_KEY,
+    consumer_secret:     process.env.TWIT_CONSUMER_SECRET,
+    access_token:        process.env.TWIT_ACCESS_TOKEN,
     access_token_secret: process.env.TWIT_ACCESS_SECRET
   });
 
@@ -129,9 +142,9 @@ r = repl.start({
   prompt: 'doujinshi_txt>'
 });
 
-r.context.startingWords = startingWords;
-r.context.markovChain = markovChain;
-r.context.getMarkovString = getMarkovString;
+r.context.startingWords          = startingWords;
+r.context.markovChain            = markovChain;
+r.context.getMarkovString        = getMarkovString;
 r.context.getMarkovStringForWord = getMarkovStringForWord;
-r.context.tweetMarkovString = tweetMarkovString;
-r.context.tweetString = tweetString;
+r.context.tweetMarkovString      = tweetMarkovString;
+r.context.tweetString            = tweetString;
